49af948031fa2873da56b1975ad6ac2e37e7d47c
Michael Wisslead <michael.wisslead@gmail.com>
2016-11-23 02:43:31 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+VERSION='0.1.0'
+
+setup(
+    name='vfp2py',
+    version=VERSION,
+    description='Convert foxpro code to python',
+    author='Michael Wisslead',
+    author_email='michael.wisslead@gmail.com',
+    url='https://github.com/mwisslead',
+    packages=['vfp2py'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
