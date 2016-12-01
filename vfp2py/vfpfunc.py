9c4e9c4ee95be8e244c7a5ed3576162f2cd83deb
Michael Wisslead <michael.wisslead@gmail.com>
2016-12-01 04:14:09 -0600

    Created database framework and create_table, select, and use functions.

--- vfp2py/vfpfunc.py
+++ vfp2py/vfpfunc.py
@@ -2,6 +2,8 @@ import builtins
 import datetime
 import os
 
+import dbf
+
 SCOPES = []
 
 class Form(object):
@@ -72,3 +74,46 @@ def select(tablename):
 
 def set(setitem, *args, **kwargs):
     pass
+
+class Database_Context(object):
+    def __init__(self):
+        self.current_table = -1
+        self.open_tables = [{'name': None}]*10
+
+    def create_table(self, tablename, setup_string, free):
+        if free == 'free':
+            dbf.Table(tablename, setup_string)
+
+    def select(self, tablename):
+        if isinstance(tablename, (int, float)):
+            self.current_table = int(tablename)
+            return
+        tablenames = [t['name'] for t in self.open_tables]
+        if tablename in tablenames:
+            self.current_table = tablenames.index(tablename)
+        else:
+            raise Exception('Table is not currently open')
+
+    def use(self, tablename, workarea, opentype):
+        tablenames = [t['name'] for t in self.open_tables]
+        if tablename is None and workarea is None:
+            table = self.open_tables[self.current_table]['table']
+            table.close()
+            self.open_tables[self.current_table] = {'name': None}
+            self.current_table = -1
+            return
+        elif tablename is None:
+            ind = self.tablenames.index(workarea)
+            table = self.open_tables[ind]['table']
+            table.close()
+            self.open_tables[ind] = {'name': None}
+            if ind == self.current_table:
+                self.current_table = -1
+            return
+        table = dbf.Table(tablename)
+        table.open()
+        if workarea == 0:
+            workarea = tablenames.index(None)
+        self.open_tables[workarea] = {'name': tablename, 'table': table}
+
+db = Database_Context()
