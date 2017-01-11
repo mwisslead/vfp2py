0a7c22169825611659b7796de4e9090d616dc683
Michael Wisslead <michael.wisslead@gmail.com>
2017-01-10 19:32:18 -0600

    Add inlined EMPTY function.

--- vfp2py/__main__.py
+++ vfp2py/__main__.py
@@ -640,6 +640,8 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
             else:
                 args[1] = self.to_int(args[1])
                 return self.add_args_to_code('{}.alen({})', args)
+        if funcname == 'empty':
+            return self.add_args_to_code('(not {} if {} is not None else False)', args + args)
         if funcname in ('repli', 'replicate'):
             args[1:] = [self.to_int(arg) for arg in args[1:]]
             return self.add_args_to_code('({} * {})', args)
