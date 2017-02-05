4e4e123a29f87700d7766e69d11f37e53c68823d
Michael Wisslead <michael.wisslead@gmail.com>
2017-02-05 03:13:39 -0600

    Add more codes and bug fixes in fxp_read.

--- vfp2py/fxp_read.py
+++ vfp2py/fxp_read.py
@@ -2,6 +2,7 @@ import sys
 import struct
 from math import floor, log10
 from datetime import datetime
+from collections import OrderedDict
 
 HEADER_SIZE = 0x29
 
@@ -33,6 +34,9 @@ def read_double(fid):
 def read_alias(fid):
     return 'NAME {}.'.format(read_ushort(fid))
 
+def read_special_alias(fid):
+    return 'SPECIAL_NAME {}.'.format(fid.read(1)[0])
+
 def read_name(fid):
     return 'NAME {}'.format(read_ushort(fid))
 
@@ -51,10 +55,17 @@ def read_expr(fid):
             if callable(code):
                 code = code(fid)
         except:
-            code = 'FUNCTION {:02x}'.format(code)
+            code = 'FUNCTION {:02x}'.format(codeval)
         expr.append(code)
         codeval = fid.read(1)[0]
     return expr
+
+def read_setcode(fid, length):
+    try:
+        return [SETCODES[fid.read(1)[0]]]
+    except:
+        fid.seek(fid.tell() - 1)
+        return ['{:02x}'.format(fid.read(1)[0])]
 
 class Token(object):
     def __init__(self, tokenstr, tokenval):
@@ -64,6 +75,11 @@ class Token(object):
     def __repr__(self):
         return repr('{}({})'.format(self.str, self.val))
 
+SPECIAL_NAMES = {
+    0x39: '_SCREEN',
+    0x43: '_VFP',
+}
+
 COMMANDS = {
     #Commands are identified by a single byte as shown in the following list:
     0x01: lambda fid, length: ['RAW CODE', read_raw(fid, length)],
@@ -72,12 +88,14 @@ COMMANDS = {
     0x0C: 'CASE',
     0x0E: 'CLEAR',
     0x0F: 'CLOSE',
+    0x14: 'DELETE',
     0x18: 'DO',
-    0x18: lambda fid, length: ['DO', CLAUSES[fid.read(1)[0]]],
+    #0x18: lambda fid, length: ['DO', CLAUSES[fid.read(1)[0]]],
     0x1B: 'ELSE',
     0x1C: 'ENDCASE',
     0x1D: 'ENDDO',
     0x1E: 'ENDIF',
+    0x20: 'ERASE',
     0x23: 'GO',
     0x25: 'IF',
     0x2D: 'LOCATE',
@@ -90,7 +108,7 @@ COMMANDS = {
     0x42: 'RETURN',
     0x45: 'SEEK',
     0x46: 'SELECT',
-    0x47: lambda fid, length: ['SET', SETCODES[fid.read(1)[0]]],
+    0x47: read_setcode,
     0x48: 'SKIP',
     0x4A: 'STORE',
     0x51: 'USE',
@@ -99,6 +117,7 @@ COMMANDS = {
     0x7C: 'DECLARE',
     0x7E: 'SCAN',
     0x7F: 'SCAN',
+    0x83: 'COMPILE',
     0x84: 'FOR',
     0x85: 'ENDFOR',
     0x86: 'expression',
@@ -117,11 +136,13 @@ COMMANDS = {
 }
 
 SETCODES = {
-    0x28: 'ORDER (SET)',
-    0x2B: 'PROCEDURE (SET)'
+    0x62: 'SET LIBRARY',
+    0x28: 'SET ORDER',
+    0x2B: 'SET PROCEDURE'
 }
 
 TYPECODES = {
+    0x57: 'SHORT',
     0xD1: 'INTEGER',
     0xD6: 'STRING'
 }
@@ -129,18 +150,24 @@ TYPECODES = {
 CLAUSES = {
     0x01: 'ADDITIVE',
     0x02: 'ALIAS',
+    0x03: 'ALL',
+    0x12: 'FILE',
     0x13: 'FOR',
     0x14: 'FORM',
     0x16: 'IN',
     0x28: 'TO',
     0x29: 'TOP',
     0x2B: 'WHILE',
+    0x31: 'TABLE',
+    0x36: 'BOTTOM',
     0x48: 'CASE',
+    0x4B: 'PROGRAM',
     0x51: 'AS',
     0x56: 'DLLS',
     0xBE: 'PROCEDURE',
     0xC2: 'SHARED',
     0xD1: 'WITH',
+    0xD5: 'EVENTS',
     0xFC: read_expr,
     0xFD: 'END EXPR',
     0xFE: 'END COMMAND'
@@ -148,12 +175,15 @@ CLAUSES = {
 
 VALUES = {
     0x00: 'NOP',
+    0x03: 'END PAREN',
     0x04: '*',
     0x06: '+',
     0x07: ',',
+    0x08: '-',
     0x09: 'AND',
     0x0A: 'NOT',
     0x0B: 'OR',
+    0x0C: '/',
     0x0D: '<',
     0x0E: '<=',
     0x0F: '!=',
@@ -161,7 +191,9 @@ VALUES = {
     0x11: '>=',
     0x12: '>',
     0x14: '==',
+    0x18: '@',
     0xD9: read_string,
+    0xE1: read_special_alias,
     0xE2: '.',
     0xE9: read_int32,
     0xF0: lambda fid: ' '.join('{:02x}'.format(d) for d in (b'\xf1' + fid.read(2))),
@@ -182,28 +214,36 @@ FUNCTIONS = {
     0x1E: 'BOF',
     0x1F: 'CDOW',
     0x20: 'CHR',
+    0x23: 'CTOD',
     0x24: 'DATE',
     0x25: 'DAY',
+    0x2A: 'DTOC',
     0x2B: 'EOF',
     0x2D: '.F.',
     0x30: 'FILE', 
     0x34: 'FOUND',
+    0x37: 'INKEY',
     0x3D: 'LEFT',
     0x3E: 'LEN',
     0x43: 'MARK PARAMETERS',
     0x46: 'MIN',
     0x48: 'MONTH',
     0x4F: 'RECNO',
+    0x51: 'REPLICATE',
     0x52: 'RIGHT',
     0x54: 'ROUND',
     0x57: 'SELECT',
+    0x58: 'SPACE',
     0x5A: 'STR',
+    0x5C: 'SUBSTR',
     0x5D: 'SYS',
     0x61: '.T.',
     0x62: 'TYPE',
     0x66: 'UPPER',
     0x69: 'YEAR',
     0x77: 'CEILING',
+    0x91: 'FCLOSE',
+    0x95: 'FCREATE',
     0x9B: 'ALLTRIM',
     0xAA: 'USED',
     0xAB: 'BETWEEN',
@@ -222,6 +262,11 @@ EXTENDED1 = {
 
 EXTENDED2 = {
     #This list contains all those functions that are available through the 0xEA (extended function) code:
+    0x2C: 'DDEINITIATE',
+    0x2F: 'DDESETSERVICE',
+    0x30: 'DDESETTOPIC',
+    0x31: 'DDETERMINATE',
+    0x35: 'DDESETOPTION',
     0x3E: 'CAPSLOCK',
     0x4E: 'CREATEOBJECT',
     0x5A: 'ISBLANK',
@@ -243,6 +288,7 @@ EXTENDED2 = {
     0xAE: 'BITTEST',
     0xAF: 'BITCLEAR',
     0xBF: 'BINTOC',
+    0xC6: 'DIRECTORY',
     0xD9: 'VARTYPE',
     0xF0: 'STREXTRACT'
 }
@@ -256,7 +302,7 @@ def read_short(fid):
 def read_ushort(fid):
     return struct.unpack('<H', fid.read(2))[0]
 
-def read_uint(fid):
+def read_int(fid):
     return struct.unpack('<i', fid.read(4))[0]
 
 def read_uint(fid):
@@ -298,10 +344,10 @@ def read_code_line_area(fid):
             import traceback
             traceback.print_exc()
             fid.seek(file_pos)
-            import pdb
-            pdb.set_trace()
-            d.append(parse_line(fid, length-2))
-            #d.append([fid.read(length-2)])
+            #import pdb
+            #pdb.set_trace()
+            #d.append(parse_line(fid, length-2))
+            d.append(read_raw(fid, length-2))
         tot_length -= length
     return d
 
@@ -310,10 +356,11 @@ def read_code_name_list(fid):
     return [read_string(fid) for i in range(num_entries)]
 
 def change_named_value(expr, names):
+    expr = expr.split()[1]
     if expr.endswith('.'):
-        return names[int(expr[5:-1])] + '.'
+        return names[int(expr[:-1])] + '.'
     else:
-        return names[int(expr[5:])]
+        return names[int(expr)]
     return expr
 
 def concatenate_aliases(codes, names):
@@ -321,8 +368,12 @@ def concatenate_aliases(codes, names):
     new_codes = []
     while codes:
         code = codes.pop(0)
-        if hasattr(code, 'startswith') and code.startswith('NAME '):
+        if isinstance(code, str) and code.startswith('NAME '):
             code = change_named_value(code, names)
+            while code.endswith('.'):
+                code += change_named_value(codes.pop(0), names)
+        if isinstance(code, str) and code.startswith('SPECIAL_NAME '):
+            code = change_named_value(code, SPECIAL_NAMES)
             while code.endswith('.'):
                 code += change_named_value(codes.pop(0), names)
         new_codes.append(code)
@@ -353,7 +404,7 @@ def read_procedure_header(fid):
     code_pos = read_uint(fid) + HEADER_SIZE
     unknown = read_short(fid)
     class_num = read_short(fid)
-    return {'name': name, 'pos': code_pos, 'class': class_num}
+    return OrderedDict((key, val) for key, val in zip(('name', 'pos', 'class'), (name, code_pos, class_num)))
 
 def read_class_header(fid):
     name = read_string(fid)
@@ -396,7 +447,7 @@ def fxp_read():
         print(date)
 
         fid.seek(procedure_pos, 0)
-        procedures = [{'name': '', 'pos': 0x4e, 'class': -1}] + [read_procedure_header(fid) for i in range(num_procedures)]
+        procedures = [OrderedDict((key, val) for key, val in zip(('name', 'pos', 'class'), ('', 0x4e, -1)))] + [read_procedure_header(fid) for i in range(num_procedures)]
         
         for proc in procedures:
             fid.seek(proc['pos'], 0)
@@ -417,9 +468,10 @@ def fxp_read():
         for proc in procedures:
             proc.pop('class')
 
-        import json
-        print(json.dumps(procedures, indent=4))
-        print(json.dumps(classes, indent=4))
+        import pprint
+        printer = pprint.PrettyPrinter(depth=10, indent=4)
+        printer.pprint(procedures)
+        printer.pprint(classes)
 
 if __name__ == '__main__':
     fxp_read()
