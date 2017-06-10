b66a5bd64311586d211c196c2a650cf4f4bba259
Michael Wisslead <michael.wisslead@gmail.com>
2017-06-10 01:34:17 -0500

    Move bulk of __main__.py into vfp2py.py

--- vfp2py/__init__.py
+++ vfp2py/__init__.py
@@ -1,5 +1,2 @@
-from .VisualFoxpro9Lexer import VisualFoxpro9Lexer
-from .VisualFoxpro9Parser import VisualFoxpro9Parser
-from .VisualFoxpro9Visitor import VisualFoxpro9Visitor
-
-from . import vfpfunc
+import vfpfunc
+import vfp2py
