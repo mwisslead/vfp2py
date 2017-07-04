f50497f336f64d438b7e2af4f60a70cc2b798dda
Michael Wisslead <michael.wisslead@gmail.com>
2017-07-04 00:53:02 -0500

    Add inlined ADDBS function.

--- vfp2py/vfp2py_convert_visitor.py
+++ vfp2py/vfp2py_convert_visitor.py
@@ -755,7 +755,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
         if funcname == 'fseek':
             funcname = '{}.seek'.format(args[0])
             return make_func_code(funcname, *args[1:])
-        if funcname in ('file', 'directory', 'justdrive', 'justpath', 'justfname', 'juststem', 'justext', 'forceext'):
+        if funcname in ('file', 'directory', 'justdrive', 'justpath', 'justfname', 'juststem', 'justext', 'forceext', 'addbs'):
             self.imports.append('import os')
             operation = {
                 'file': [make_func_code, ['os.path.isfile'] + args],
@@ -766,6 +766,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
                 'juststem': [add_args_to_code, ('os.path.splitext(os.path.basename({}))[0]', args)],
                 'justext': [add_args_to_code, ('os.path.splitext({})[1][1:]', args)],
                 'forceext': [add_args_to_code, ('os.path.splitext({})[0] + \'.\' + {}', args)],
+                'addbs': [make_func_code, ['os.path.join'] + args + ['']],
             }[funcname]
             return operation[0](*operation[1])
         if funcname == 'set' and len(args) > 0 and string_type(args[0]):
