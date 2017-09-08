8c2bbd8e106242ad7f1f8fddb413c47f4b11e9c5
Michael Wisslead <michael.wisslead@gmail.com>
2017-09-07 23:19:34 -0500

    Add Checkbox code

--- vfp2py/vfpfunc.py
+++ vfp2py/vfpfunc.py
@@ -169,8 +169,26 @@ class Label(QtGui.QLabel, Custom):
 class Textbox(Custom):
     pass
 
-class Checkbox(Custom):
-    pass
+class Checkbox(QtGui.QCheckBox, Custom):
+    def __init__(self, *args, **kwargs):
+        QtGui.QCheckBox.__init__(self)
+        Custom.__init__(self, *args, **kwargs)
+
+    @property
+    def caption(self):
+        return self.text()
+
+    @caption.setter
+    def caption(self, val):
+        self.setText(val)
+
+    @property
+    def height(self):
+        return QtGui.QLabel.height(self)
+
+    @height.setter
+    def height(self, val):
+        self.setFixedHeight(val)
 
 class Combobox(QtGui.QComboBox, Custom):
     def __init__(self, *args, **kwargs):
