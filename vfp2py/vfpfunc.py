a5ba5a0750da15a98ec3cd45f213139158984e08
Michael Wisslead <michael.wisslead@gmail.com>
2017-10-21 16:02:13 -0500

    Add GETDIR function.

--- vfp2py/vfpfunc.py
+++ vfp2py/vfpfunc.py
@@ -987,6 +987,9 @@ def filetostr(filename):
     with open(filename) as fid:
         return fid.read().decode('ISO-8859-1')
 
+def getdir(dir='.', text='', caption='Select Text', flags=0, root_only=False):
+    return QtGui.QFileDialog.getExistingDirectory(parent=variable['_vfp'], caption=caption, dir=dir)
+
 def getfile(file_ext='', text='', button_caption='', button_type=0, title=''):
     return QtGui.QFileDialog.getOpenFileName(parent=variable['_vfp'], caption='Open', dir='.')[0]
 
