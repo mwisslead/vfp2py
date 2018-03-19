mwisslead/vfp2py

commit 6c9246b5bd10ea247480e0846d365287bd289ce0

Add locals to scoping.

vfp2py/vfp2py_convert_visitor.py
@@ -354,12 +354,13 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
         body = []
         if parameters:
             self.new_scope()
-            self.scope.update({key: False for key in parameters})
+            self.used_scope = True
+            kwargs = {p: p for p in parameters}
+            body.append(make_func_code('S.add_local', **kwargs))
         else:
             try:
                 parameter_line = next(line for line in ctx.lines().line() if not line.lineComment())
                 parameter_cmd = parameter_line.cmd()
-                keyword = parameter_cmd.PARAMETER().symbol.text.lower()
                 parameters = [self.visit_with_disabled_scope(p)[0] for p in parameter_cmd.declarationItem()]
                 lines = ctx.lines()
                 children = [c for c in lines.children if c is not parameter_line]
@@ -368,12 +369,14 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
                 for child in children:
                     lines.addChild(child)
                 self.new_scope()
+                self.used_scope = True
+                keyword = parameter_cmd.PARAMETER().symbol.text.lower()
                 if keyword.startswith('l'):
-                    self.scope.update({key: False for key in parameters})
+                    func = 'S.add_local'
                 else:
-                    self.used_scope = True
-                    decls = tuple(self.visit(p)[0] for p in parameter_cmd.declarationItem())
-                    body.append(add_args_to_code('{} = {}', (decls, tuple(parameters))))
+                    func = 'S.add_private'
+                kwargs = {p: p for p in parameters}
+                body.append(make_func_code(func, **kwargs))
             except (StopIteration, AttributeError):
                 parameters = []
                 self.new_scope()
@@ -441,8 +444,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
         return self.visit(ctx.lines())
 
     def visitForStart(self, ctx):
-        loopvar = self.visit_with_disabled_scope(ctx.idAttr())
-        self.scope[loopvar] = False
+        loopvar = self.visit(ctx.idAttr())
         if ctx.EACH():
             iterator = self.visit(ctx.expr(0))
             return add_args_to_code('for {} in {}:', (loopvar, iterator))
@@ -484,9 +486,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
         if not ctx.CATCH():
             return try_lines + finally_lines
 
-        identifier = self.visit(ctx.identifier())
-        if identifier:
-            self.scope[identifier] = False
+        identifier = add_args_to_code('S[\'{}\']', (self.visit(ctx.identifier()),))
 
         try_lines = [CodeStr('try:'), try_lines]
 
@@ -498,9 +498,6 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
         catch_lines.append(self.visit(ctx.catchLines))
 
         finally_lines = [CodeStr('finally:'), finally_lines] if finally_lines else []
-
-        if ctx.identifier():
-            del self.scope[identifier]
 
         return try_lines + catch_lines + finally_lines
 
@@ -525,16 +522,11 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
             inds = [ind or (1,) for ind in inds]
 
         if scope in ('hidden', 'protected'):
-            names = [add_args_to_code('self.{}', [name]) for name in names]
-        elif scope == 'local':
-            for name in names:
-                self.scope[name] = False
-        else:
-            self.used_scope = True
+            names = [add_args_to_code('self.{}', [name]) if not keyword.iskeyword(name) else CodeStr('getattr(self, {}'.format(name)) for name in names]
 
         arrays = [(name, make_func_code('Array', *ind)) for name, ind in zip(names, inds) if ind]
 
-        if scope in ('public', 'private'):
+        if scope in ('public', 'private', 'local'):
             func = 'S.add_'  + scope
             kwargs = {str(name): array for name, array in arrays}
             names = [str(name) for name, ind in zip(names, inds) if not ind]
@@ -544,7 +536,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
             return make_func_code(func, *names, **kwargs)
         else:
             names = [name for name, ind in zip(names, inds) if not ind]
-            return [CodeStr(' = '.join(repr(arg) for arg in (names + [False])) + ' #LOCAL Declaration')] if names else [] + \
+            return [CodeStr(' = '.join(repr(arg) for arg in (names + [False])))] if names else [] + \
                    [add_args_to_code('{} = {}', (name, array)) for name, array in arrays]
 
     def visitDeclarationItem(self, ctx):

testbed/test_lib.py
@@ -39,27 +39,27 @@ def used_tests():
 
 def date_tests():
     S.pushscope()
-    somedate = False  # LOCAL Declaration
-    somedate = dt.date(2017, 6, 30)
-    assert somedate == dt.date(2017, 6, 30)
-    assert vfpfunc.dow_fix(somedate.weekday()) == 6
-    assert somedate.strftime('%A') == 'Friday'
-    assert somedate.month == 6
-    assert somedate.strftime('%B') == 'June'
-    assert somedate.strftime('%d %B %Y') == '30 June 2017'
+    S.add_local('somedate')
+    S['somedate'] = dt.date(2017, 6, 30)
+    assert S['somedate'] == dt.date(2017, 6, 30)
+    assert vfpfunc.dow_fix(S['somedate'].weekday()) == 6
+    assert S['somedate'].strftime('%A') == 'Friday'
+    assert S['somedate'].month == 6
+    assert S['somedate'].strftime('%B') == 'June'
+    assert S['somedate'].strftime('%d %B %Y') == '30 June 2017'
     assert len(dt.datetime.now().time().strftime('%H:%M:%S')) == 8
     assert len(dt.datetime.now().time().strftime('%H:%M:%S.%f')[:11]) == 11
-    assert dt.datetime.combine(somedate, dt.datetime.min.time()) == dt.datetime(2017, 6, 30, 0)
-    assert vfpfunc.gomonth(somedate, -4) == dt.date(2017, 2, 28)
-    assert vfpfunc.vartype(somedate) == 'D'
-    assert vfpfunc.vartype(dt.datetime.combine(somedate, dt.datetime.min.time())) == 'T'
+    assert dt.datetime.combine(S['somedate'], dt.datetime.min.time()) == dt.datetime(2017, 6, 30, 0)
+    assert vfpfunc.gomonth(S['somedate'], -4) == dt.date(2017, 2, 28)
+    assert vfpfunc.vartype(S['somedate']) == 'D'
+    assert vfpfunc.vartype(dt.datetime.combine(S['somedate'], dt.datetime.min.time())) == 'T'
     S.popscope()
 
 
 def math_tests():
     S.pushscope()
-    num_value = False  # LOCAL Declaration
-    num_value = math.pi
+    S.add_local('num_value')
+    S['num_value'] = math.pi
     assert round(math.pi, 2) == 3.14
     assert abs(math.tan(math.radians(45)) - 1) < 0.001
     assert abs(math.sin(math.radians(90)) - 1) < 0.001
@@ -67,10 +67,10 @@ def math_tests():
     assert abs(math.cos(math.radians(45)) - math.sqrt(2) / 2) < 0.001
     assert 0 < random.random() and random.random() < 1
 
-    stringval = False  # LOCAL Declaration
-    stringval = '1e5'
-    assert float(stringval) == 100000
-    assert vfpfunc.vartype(num_value) == 'N'
+    S.add_local('stringval')
+    S['stringval'] = '1e5'
+    assert float(S['stringval']) == 100000
+    assert vfpfunc.vartype(S['num_value']) == 'N'
     S.popscope()
 
 
@@ -140,27 +140,27 @@ def misc_tests():
 
 def _add_db_record(seed=False):
     S.pushscope()
-    fake = fake_name = fake_st = fake_quantity = fake_received = False  # LOCAL Declaration
-    fake = faker.Faker()
-    fake.seed(seed)
-    fake_name = fake.name()
-    fake_st = fake.state_abbr()
-    fake_quantity = fake.random_int(0, 100)
-    fake_received = fake.boolean()
-    DB.insert('report', (fake_name, fake_st, fake_quantity, fake_received))
+    S.add_local('fake', 'fake_name', 'fake_st', 'fake_quantity', 'fake_received')
+    S['fake'] = faker.Faker()
+    S['fake'].seed(seed)
+    S['fake_name'] = S['fake'].name()
+    S['fake_st'] = S['fake'].state_abbr()
+    S['fake_quantity'] = S['fake'].random_int(0, 100)
+    S['fake_received'] = S['fake'].boolean()
+    DB.insert('report', (S['fake_name'], S['fake_st'], S['fake_quantity'], S['fake_received']))
     S.popscope()
 
 
 def _sqlexec_add_record(sqlconn=False, seed=False):
     S.pushscope()
-    fake = fake_name = fake_st = fake_quantity = fake_received = False  # LOCAL Declaration
-    fake = faker.Faker()
-    fake.seed(seed)
-    fake_name = fake.name()
-    fake_st = fake.state_abbr()
-    fake_quantity = fake.random_int(0, 100)
-    fake_received = fake.boolean()
-    S['sqlcmd'] = "insert into REPORT values ('" + fake_name + "','" + fake_st + "'," + vfpfunc.num_to_str(fake_quantity).strip() + ',' + vfpfunc.num_to_str(int(fake_received)).strip() + ')'
+    S.add_local('fake', 'fake_name', 'fake_st', 'fake_quantity', 'fake_received')
+    S['fake'] = faker.Faker()
+    S['fake'].seed(seed)
+    S['fake_name'] = S['fake'].name()
+    S['fake_st'] = S['fake'].state_abbr()
+    S['fake_quantity'] = S['fake'].random_int(0, 100)
+    S['fake_received'] = S['fake'].boolean()
+    S['sqlcmd'] = "insert into REPORT values ('" + S['fake_name'] + "','" + S['fake_st'] + "'," + vfpfunc.num_to_str(S['fake_quantity']).strip() + ',' + vfpfunc.num_to_str(int(S['fake_received'])).strip() + ')'
     print(S['sqlcmd'])
     return S.popscope(vfpfunc.sqlexec(sqlconn, S['sqlcmd']))
 
@@ -190,30 +190,30 @@ def database_tests():
         assert DB.field(2) == 'st'
         assert not DB.found()
         DB.goto(None, 0)
-        loopcount = False  # LOCAL Declaration
-        loopcount = 0
+        S.add_local('loopcount')
+        S['loopcount'] = 0
         for _ in DB.scanner(scope=('rest',)):
             assert len(S['name'].strip()) > 0
-            loopcount += 1
-        assert loopcount == 4
+            S['loopcount'] += 1
+        assert S['loopcount'] == 4
         DB.goto(None, 3)
-        loopcount = 0
+        S['loopcount'] = 0
         for _ in DB.scanner(scope=('all',), condition=lambda: S['st'].strip() == 'ID'):
             assert len(S['name'].strip()) > 0
-            loopcount += 1
-        assert loopcount == 2
-        loopcount = 0
+            S['loopcount'] += 1
+        assert S['loopcount'] == 2
+        S['loopcount'] = 0
         for _ in DB.scanner(scope=('rest',), condition=lambda: S['st'].strip() == 'ID'):
             assert len(S['name'].strip()) > 0
-            loopcount += 1
-        assert loopcount == 0
+            S['loopcount'] += 1
+        assert S['loopcount'] == 0
         DB.goto(None, 0)
-        loopcount = 0
+        S['loopcount'] = 0
         for _ in DB.scanner(scope=('rest',), condition=lambda: S['st'].strip() == 'ID'):
             assert len(S['name'].strip()) > 0
-            loopcount += 1
-        assert loopcount == 2
-        del loopcount
+            S['loopcount'] += 1
+        assert S['loopcount'] == 2
+        del S['loopcount']
         assert S['name'].strip() == 'Norma Fisher', S['name'].strip() + ' should be Norma Fisher'
         assert DB.recno() == 1
         DB.goto(None, -1)

testbed/conversion.vfp2py
@@ -109,36 +109,36 @@ ACTIVATE MENU test NOWAIT
 # comment
 # continued on another line
 # and another
-string_val = float_val = money_val = int_val = blob_val = bool_val = null_val = nulldate_val = date_val = datetime_val = obj_val = False  # LOCAL Declaration
-y = z = w = False  # LOCAL Declaration
-string_val = 'str'
-float_val = 3.0
-float_val = .5e-5
-float_val = 8e+18
-float_val = 1.3e12
-float_val = .1e0
-money_val = 3.5123
-int_val = 3
-int_val = 0x3
-int_val = 0x3f
-int_val = 0x0
-blob_val = bytearray(b'\x124Vx\x9a\xbc\xde')
-blob_val = bytearray(b'\x00\x124Vx\x9a\xbc\xde')
-blob_val = bytearray(b'')
-bool_val = False
-null_val = None
-nulldate_val = None
-nulldate_val = None
-date_val = dt.date(2017, 5, 6)
-datetime_val = dt.datetime(2017, 5, 6, 17)
-print((float_val + int_val + int_val + float_val + int_val - float_val) / 3 / 4 * -5 - int_val * 3)
+S.add_local('string_val', 'float_val', 'money_val', 'int_val', 'blob_val', 'bool_val', 'null_val', 'nulldate_val', 'date_val', 'datetime_val', 'obj_val')
+S.add_local('y', 'z', 'w')
+S['string_val'] = 'str'
+S['float_val'] = 3.0
+S['float_val'] = .5e-5
+S['float_val'] = 8e+18
+S['float_val'] = 1.3e12
+S['float_val'] = .1e0
+S['money_val'] = 3.5123
+S['int_val'] = 3
+S['int_val'] = 0x3
+S['int_val'] = 0x3f
+S['int_val'] = 0x0
+S['blob_val'] = bytearray(b'\x124Vx\x9a\xbc\xde')
+S['blob_val'] = bytearray(b'\x00\x124Vx\x9a\xbc\xde')
+S['blob_val'] = bytearray(b'')
+S['bool_val'] = False
+S['null_val'] = None
+S['nulldate_val'] = None
+S['nulldate_val'] = None
+S['date_val'] = dt.date(2017, 5, 6)
+S['datetime_val'] = dt.datetime(2017, 5, 6, 17)
+print((S['float_val'] + S['int_val'] + S['int_val'] + S['float_val'] + S['int_val'] - S['float_val']) / 3 / 4 * -5 - S['int_val'] * 3)
 print('\x03')
-print(chr(int(int_val)))
+print(chr(int(S['int_val'])))
 print('   ')
 print(20 * ' ')
-print(int(int_val) * ' ')
-print(date_val.day)
-print(vfpfunc.dow_fix(date_val.weekday()))
+print(int(S['int_val']) * ' ')
+print(S['date_val'].day)
+print(vfpfunc.dow_fix(S['date_val'].weekday()))
 print('chr(65) = A, just letting you know.\r\n')
 print((2 ** 3) ** 4)
 print(2 ** 3 ** 4)
@@ -148,22 +148,22 @@ print(10 >= 5)
 print(10 <= 5)
 print(10 <= 5)
 print(True or False and False or False)
-print(S['x'] or y and w or z)
-print(bytearray(string_val))
-print(float(float_val))
-obj_val = vfpfunc.create_object('Test')
-obj_val = vfpfunc.Form()
-del string_val, int_val, bool_val, null_val
-items = Array(3, 5)
-item = False  # LOCAL Declaration
-for item in items:
-    if not item:
+print(S['x'] or S['y'] and S['w'] or S['z'])
+print(bytearray(S['string_val']))
+print(float(S['float_val']))
+S['obj_val'] = vfpfunc.create_object('Test')
+S['obj_val'] = vfpfunc.Form()
+del S['string_val'], S['int_val'], S['bool_val'], S['null_val']
+S.add_local(items=Array(3, 5))
+S.add_local('item')
+for S['item'] in S['items']:
+    if not S['item']:
         continue
         # IF
         # TEST()
         # ENDIF
     break
-del items, item
+del S['items'], S['item']
 # line comment1
 # line comment2
 if S['x'] == 1:
@@ -296,6 +296,7 @@ DEFINE CLASS SUBOBJ2 AS SUBOBJ
 ENDDEFINE
 
 DEFINE CLASS TESTCLASS AS COMMANDBUTTON
+   PROTECTED SOMEVAR
    ADD OBJECT TEST1 as custom
    ADD OBJECT TEST2 as subobj WITH X = 4
    ADD OBJECT TEST3 as unknownobj WITH X = '4'
@@ -348,8 +349,11 @@ class Subobj(vfpfunc.Custom):
 
         # comment
     def init(self, x=False):
+        S.pushscope()
+        S.add_local(x=x)
         super(type(self), self).init()
-        self.x = x
+        self.x = S['x']
+        S.popscope()
 
 # comment about subobj2
 
@@ -364,14 +368,18 @@ class Subobj2(Subobj):
 class Testclass(vfpfunc.Commandbutton):
 
     def _assign(self, *args, **kwargs):
+        S.pushscope()
         vfpfunc.Commandbutton._assign(self)
         self.test1 = vfpfunc.Custom(name='test1', parent=self)
         self.test2 = Subobj(x=4, name='test2', parent=self)
         self.test3 = vfpfunc.create_object('Unknownobj', x='4', name='test3', parent=self)
+        S.popscope()
 
     def init(self, x=False):
+        S.pushscope()
+        S.add_local(x=x)
         # FIX ME: NODEFAULT
-        pass
+        S.popscope()
 
 
 class Abutton(Testclass):
@@ -384,9 +392,12 @@ class Abutton(Testclass):
 
 
 def random_function(x=False):
+    S.pushscope()
+    S.add_local(x=x)
 
     # something
-    print(x)
+    print(S['x'])
+    S.popscope()
 
 # comment about testclass2
 
@@ -399,7 +410,7 @@ class Testclass2(vfpfunc.classes['Unknownclass']):
 
 def another_random_function(x=False, y=False):
     S.pushscope()
-    (S['x'], S['y']) = (x, y)
+    S.add_private(y=y, x=x)
 
     # something
     print(S['x'])
@@ -429,15 +440,15 @@ delete file test.txt recycle
 @end=vfp@
 
 @begin=python@
-a = False  # LOCAL Declaration
+S.add_local('a')
 F['a']()
 F['a+b']()
-F[a + S['b']]()
-F[a.strip()]()
+F[S['a'] + S['b']]()
+F[S['a'].strip()]()
 a.test()
-vfpfunc.module(a).test()
-vfpfunc.module(a + '.PRG').test()
-vfpfunc.module(a + (S['b'])).test()
+vfpfunc.module(S['a']).test()
+vfpfunc.module(S['a'] + '.PRG').test()
+vfpfunc.module(S['a'] + (S['b'])).test()
 test._program_main()
 test.test()
 vfpfunc.do_form('splash.scx')
@@ -462,13 +473,13 @@ rd alltrim(test)
 @end=vfp@
 
 @begin=python@
-test = False  # LOCAL Declaration
-shutil.copyfile(test, 'tset')
-shutil.move(test, 'tset')
-os.mkdir(test - test)
+S.add_local('test')
+shutil.copyfile(S['test'], 'tset')
+shutil.move(S['test'], 'tset')
+os.mkdir(S['test'] - S['test'])
 os.mkdir('test+test')
-os.rmdir(test + test)
-os.rmdir(test.strip())
+os.rmdir(S['test'] + S['test'])
+os.rmdir(S['test'].strip())
 subprocess.call(['ls', '-al', pathname])
 @end=python@
 
@@ -488,13 +499,13 @@ update test set a=b, c=d, e=f where x=3
 @begin=python@
 DB.create_cursor('test_cursor', 'somefield n(3)', '')
 DB.continue_locate()
-search_for = countval = sumval = False  # LOCAL Declaration
-search_for = 'PAUL'
-DB.seek(None, search_for.strip())
-countval = DB.count(None, ('all',), for_cond=lambda: S['test'] == 3)
-sumval = DB.sum(None, ('all',), lambda: S['t'] * S['t'], for_cond=lambda: S['t'] > 0)
+S.add_local('search_for', 'countval', 'sumval')
+S['search_for'] = 'PAUL'
+DB.seek(None, S['search_for'].strip())
+S['countval'] = DB.count(None, ('all',), for_cond=lambda: S['test'] == 3)
+S['sumval'] = DB.sum(None, ('all',), lambda: S['t'] * S['t'], for_cond=lambda: S['t'] > 0)
 DB.locate(nooptimize=True, while_cond=lambda: S['x'] > 5)
-del search_for, countval, sumval
+del S['search_for'], S['countval'], S['sumval']
 DB.update('test', [('a', S['b']), ('c', S['d']), ('e', S['f'])], where=lambda: S['x'] == 3)
 @end=python@
 
@@ -566,11 +577,11 @@ ASSERT X = .T. MESSAGE Y + ' ASSERT'
 @end=vfp@
 
 @begin=python@
-x = y = False  # LOCAL Declaration
-x = False
-y = 'failed'
-assert not x
-assert x == True, y + ' ASSERT'
+S.add_local('x', 'y')
+S['x'] = False
+S['y'] = 'failed'
+assert not S['x']
+assert S['x'] == True, S['y'] + ' ASSERT'
 @end=python@
 
 @begin=vfp@&&lines
@@ -655,28 +666,28 @@ RELEASE MYFILE, MYDIR
 @end=vfp@
 
 @begin=python@
-myfile = mydir = False  # LOCAL Declaration
-myfile = 'c:\\test\\test.prg'
-mydir = 'c:\\test\\test\\dir'
-print(os.path.isfile(myfile))
-print(os.path.splitdrive(myfile)[0])
-print(os.path.dirname(myfile))
-print(os.path.basename(myfile))
-print(os.path.splitext(os.path.basename(myfile))[0])
-print(os.path.splitext(myfile)[1][1:])
-print(os.path.splitext(myfile)[0] + '.' + 'py')
-print(os.path.isdir(mydir))
-print(os.path.splitdrive(mydir)[0])
-print(os.path.dirname(mydir))
-print(os.path.basename(mydir))
-print(os.path.splitext(os.path.basename(mydir))[0])
-print(os.path.splitext(mydir)[1][1:])
-print(os.path.splitext(mydir)[0] + '.' + 'py')
-print(os.path.join(mydir, 'dir1'))
-print(os.path.join(mydir, 'dir1', 'dir2'))
-print(os.path.join(mydir, 'dir1', 'dir2', 'dir3'))
+S.add_local('myfile', 'mydir')
+S['myfile'] = 'c:\\test\\test.prg'
+S['mydir'] = 'c:\\test\\test\\dir'
+print(os.path.isfile(S['myfile']))
+print(os.path.splitdrive(S['myfile'])[0])
+print(os.path.dirname(S['myfile']))
+print(os.path.basename(S['myfile']))
+print(os.path.splitext(os.path.basename(S['myfile']))[0])
+print(os.path.splitext(S['myfile'])[1][1:])
+print(os.path.splitext(S['myfile'])[0] + '.' + 'py')
+print(os.path.isdir(S['mydir']))
+print(os.path.splitdrive(S['mydir'])[0])
+print(os.path.dirname(S['mydir']))
+print(os.path.basename(S['mydir']))
+print(os.path.splitext(os.path.basename(S['mydir']))[0])
+print(os.path.splitext(S['mydir'])[1][1:])
+print(os.path.splitext(S['mydir'])[0] + '.' + 'py')
+print(os.path.join(S['mydir'], 'dir1'))
+print(os.path.join(S['mydir'], 'dir1', 'dir2'))
+print(os.path.join(S['mydir'], 'dir1', 'dir2', 'dir3'))
 print(os.getcwd())
-del myfile, mydir
+del S['myfile'], S['mydir']
 @end=python@
 
 @begin=vfp@&&lines
@@ -691,14 +702,14 @@ pydict.setitem('one', 1)
 @end=vfp@
 
 @begin=python@
-somearray = Array(2, 5)
-pytuple = pylist = pydict = False  # LOCAL Declaration
-pytuple = ('a', 3, True)
-pylist = somearray.data[:]
-pylist.append('appended value')
-pydict = {}
-pydict['one'] = 1
-print(pydict['one'])
+S.add_local(somearray=Array(2, 5))
+S.add_local('pytuple', 'pylist', 'pydict')
+S['pytuple'] = ('a', 3, True)
+S['pylist'] = S['somearray'].data[:]
+S['pylist'].append('appended value')
+S['pydict'] = {}
+S['pydict']['one'] = 1
+print(S['pydict']['one'])
 @end=python@
 
 @begin=vfp@&&lines
@@ -731,8 +742,8 @@ throw 'Error' + ' Message'
 @begin=python@
 try:
     assert False
-except Exception as oerr:
-    oerr = vfpfunc.Exception.from_pyexception(oerr)
+except Exception as S['oerr']:
+    S['oerr'] = vfpfunc.Exception.from_pyexception(S['oerr'])
     raise
 raise Exception('Error Message')
 @end=python@
@@ -747,12 +758,12 @@ T = aINS(main_array, 3, 2)
 @end=vfp@
 
 @begin=python@
-cnt_fields = False  # LOCAL Declaration
-main_array = Array(1)
-cnt_fields = DB.afields(None, 'main_array', (locals(), S)) + 32
-cnt_fields = DB.afields('report', 'main_array2', (locals(), S)) + 47
-S['t'] = main_array.insert(None, 3)
-S['t'] = main_array.insert(None, 3, 2)
+S.add_local('cnt_fields')
+S.add_local(main_array=Array(1))
+S['cnt_fields'] = DB.afields(None, 'main_array', (locals(), S)) + 32
+S['cnt_fields'] = DB.afields('report', 'main_array2', (locals(), S)) + 47
+S['t'] = S['main_array'].insert(None, 3)
+S['t'] = S['main_array'].insert(None, 3, 2)
 @end=python@
 
 @begin=vfp@&&lines

commit ec40da9c9dbac15ec74648a18e76efae8eb7ac0b

Fix bug introduced in commit 6c9246b

vfp2py/vfp2py_convert_visitor.py
@@ -361,6 +361,7 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
             try:
                 parameter_line = next(line for line in ctx.lines().line() if not line.lineComment())
                 parameter_cmd = parameter_line.cmd()
+                keyword = parameter_cmd.PARAMETER().symbol.text.lower()
                 parameters = [self.visit_with_disabled_scope(p)[0] for p in parameter_cmd.declarationItem()]
                 lines = ctx.lines()
                 children = [c for c in lines.children if c is not parameter_line]
@@ -370,7 +371,6 @@ class PythonConvertVisitor(VisualFoxpro9Visitor):
                     lines.addChild(child)
                 self.new_scope()
                 self.used_scope = True
-                keyword = parameter_cmd.PARAMETER().symbol.text.lower()
                 if keyword.startswith('l'):
                     func = 'S.add_local'
                 else:

testbed/test_lib.py
@@ -140,9 +140,10 @@ def misc_tests():
 
 def _add_db_record(seed=False):
     S.pushscope()
+    S.add_local(seed=seed)
     S.add_local('fake', 'fake_name', 'fake_st', 'fake_quantity', 'fake_received')
     S['fake'] = faker.Faker()
-    S['fake'].seed(seed)
+    S['fake'].seed(S['seed'])
     S['fake_name'] = S['fake'].name()
     S['fake_st'] = S['fake'].state_abbr()
     S['fake_quantity'] = S['fake'].random_int(0, 100)
@@ -153,16 +154,17 @@ def _add_db_record(seed=False):
 
 def _sqlexec_add_record(sqlconn=False, seed=False):
     S.pushscope()
+    S.add_local(sqlconn=sqlconn, seed=seed)
     S.add_local('fake', 'fake_name', 'fake_st', 'fake_quantity', 'fake_received')
     S['fake'] = faker.Faker()
-    S['fake'].seed(seed)
+    S['fake'].seed(S['seed'])
     S['fake_name'] = S['fake'].name()
     S['fake_st'] = S['fake'].state_abbr()
     S['fake_quantity'] = S['fake'].random_int(0, 100)
     S['fake_received'] = S['fake'].boolean()
     S['sqlcmd'] = "insert into REPORT values ('" + S['fake_name'] + "','" + S['fake_st'] + "'," + vfpfunc.num_to_str(S['fake_quantity']).strip() + ',' + vfpfunc.num_to_str(int(S['fake_received'])).strip() + ')'
     print(S['sqlcmd'])
-    return S.popscope(vfpfunc.sqlexec(sqlconn, S['sqlcmd']))
+    return S.popscope(vfpfunc.sqlexec(S['sqlconn'], S['sqlcmd']))
 
 
 def database_tests():
@@ -176,10 +178,10 @@ def database_tests():
         try:
             DB.use('report', 0, 'shared')
             assert False
-        except Exception as oerr:
-            oerr = vfpfunc.Exception.from_pyexception(oerr)
-            print(oerr.message)
-            assert oerr.message == 'File is in use.'
+        except Exception as S['oerr']:
+            S['oerr'] = vfpfunc.Exception.from_pyexception(S['oerr'])
+            print(S['oerr'].message)
+            assert S['oerr'].message == 'File is in use.'
         _add_db_record(0)
         _add_db_record(1)
         _add_db_record(2)
@@ -263,9 +265,9 @@ def database_tests():
         assert DB.fcount() == 4
         DB.use(None, None, None)
         os.remove('report2.dbf')
-    except Exception as err:
-        err = vfpfunc.Exception.from_pyexception(err)
-        print(err.message)
+    except Exception as S['err']:
+        S['err'] = vfpfunc.Exception.from_pyexception(S['err'])
+        print(S['err'].message)
         DB.browse()
         raise
     finally: